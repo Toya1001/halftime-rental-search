e869cff0ec5dc48877b312a3923803c5bfcc1555
Latoya Jackson <jacksonlatoya04@gmail.com>
2022-02-03 11:07:16 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from "react"
+import PropTypes from "prop-types";
 import './App.css';
+import cars from "./cars.json";
+
+const CarsRow=( {cars})=>(
+  <tr key={cars.ID}>
+          <td>{cars.Manufacturer}</td>
+          <td>{cars.Model}</td>
+        </tr>
+)
+
+CarsRow.propTypes = {
+  cars: PropTypes.shape({
+    Manufacturer: PropTypes.string,
+     Model: PropTypes.string
+  }),
+ 
+};
 
 function App() {
+  const [filter, filterSet] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    style={{ 
+      margin: "auto",
+      width:800,
+      paddingTop: "1rem",
+     }}
+    >
+      <h1 className="title"> Halftime Rental Search</h1>
+      <input
+      value = {filter}
+      onChange={(evt) => filterSet(evt.target.value)}
+      />
+    <table width="100%">
+      <thead>
+        <tr>
+          <th>Manufacturer </th>
+          <th>Model</th>
+        </tr>
+      </thead>
+      <tbody>
+        { cars 
+        .filter(
+          (cars) => cars.Model.toLowerCase().includes(filter.toLowerCase()))
+        .map((cars)=>(
+          <CarsRow cars = {cars} key={cars.ID}/>
+          
+        ))}
+      </tbody>
+      
+    </table>
     </div>
   );
 }
